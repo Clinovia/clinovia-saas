4043d32b0b7341f2b0e3d5613789511dda44e1f1
Sophie Choe <sophiechoe@Sophies-MacBook-Air.local>
2025-12-07 19:10:01 +0900

    added more dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,9 @@
-/** next.config.js */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "your-domain.com",
-      },
-    ],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 }
 
